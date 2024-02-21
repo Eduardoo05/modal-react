ec0958783490d9ed9aeb33d731632fc60f351ccf
Elvin Eduardo <franco193@hotmail.com>
2024-02-21 19:24:26 -0400

    se creó el primer modal

---
+++ src/components/Modal.jsx
@@ -0,0 +1,26 @@
+import { useRef } from "react"
+
+export default function Modal({children, title, root}){
+    const ref = useRef(null)
+    
+  return (
+    <>
+    <div ref={ref}>
+        <div>
+        <div>
+            <div>Titulo</div>
+            <div>
+                <button>X</button>
+            </div>
+        </div>
+        <div>
+            {children} 
+        </div>
+
+        </div>
+    </div>
+    </>
+  )
+}
+
+ 
