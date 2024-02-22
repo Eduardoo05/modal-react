157329448435b8172c8f09c741c896e1685db6f3
Elvin Eduardo <franco193@hotmail.com>
2024-02-22 11:40:02 -0400

    se creó el modal de manera funcional, se eliminaron estilos predeterminados de react y se agregaron estilos por medio de modulos en css

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,19 +1,20 @@
 import { useRef } from "react"
+import styles from './modal.module.scss'
 
 export default function Modal({children, title, root}){
     const ref = useRef(null)
     
   return (
     <>
-    <div ref={ref}>
-        <div>
-        <div>
+    <div ref={ref} className={styles.modalContainer}>
+        <div className={styles.modalView}>
+        <div className={styles.modalHeader}>
             <div>Titulo</div>
             <div>
-                <button>X</button>
+                <button className={styles.closeButton}>X</button>
             </div>
         </div>
-        <div>
+        <div className={styles.modalContent}>
             {children} 
         </div>
 
